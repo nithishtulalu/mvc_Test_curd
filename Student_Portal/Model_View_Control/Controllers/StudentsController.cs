4e0fb97bb8a06e57aed629a1a01343a0003f99c5
nithish <nithishtulalu121@gmail.com>
2025-04-10 19:58:58 +0530

    final commit

--- Student_Portal/Model_View_Control/Controllers/StudentsController.cs
+++ Student_Portal/Model_View_Control/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Model_View_Control.Data;
 using Model_View_Control.Models;
 using Model_View_Control.Models.Entity;
@@ -28,9 +29,56 @@ namespace Model_View_Control.Controllers
                 Phone = viewModel.Phone,
                 Subscribed = viewModel.Subscribed
             };
-             await _context.Students.AddAsync(student);
-             await  _context.SaveChangesAsync();
-             return View();
+            await _context.Students.AddAsync(student);
+            await _context.SaveChangesAsync();
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var data = await _context.Students.ToListAsync();
+            return View(data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var data = await _context.Students.FindAsync(id);
+
+            return View(data);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Student view)
+        {
+            var data = await _context.Students.FindAsync(view.Id);
+            if (data is not null)
+            {
+                data.Name = view.Name;
+                data.email = view.email;
+                data.Phone = view.Phone;
+                data.Subscribed = view.Subscribed;
+                await _context.SaveChangesAsync();
+
+            }
+            return RedirectToAction("List", "Students");
+
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Student view)
+        {
+            var data = await _context.Students.AsNoTracking().FirstOrDefaultAsync(x => x.Id == view.Id);
+
+            if (data is not null)
+            {
+                _context.Students.Remove(view);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
         }
     }
-}
+
+ }
